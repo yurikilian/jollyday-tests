87474a6013c00160bb22475aa8a0001b29abe5e4
Yuri Kilian <kilian.inf@gmail.com>
2017-03-12 23:55:12 -0300

    Adicionado prefixer do jaxb

--- src/test/java/inf/kilian/holyday/HolidayManagerTest.java
+++ src/test/java/inf/kilian/holyday/HolidayManagerTest.java
@@ -1,5 +1,9 @@
 package inf.kilian.holyday;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
@@ -7,6 +11,8 @@ import javax.xml.bind.Marshaller;
 
 import org.junit.Test;
 
+import de.jollyday.HolidayManager;
+import de.jollyday.ManagerParameters;
 import de.jollyday.config.ChristianHoliday;
 import de.jollyday.config.ChristianHolidayType;
 import de.jollyday.config.Configuration;
@@ -18,7 +24,7 @@ import de.jollyday.config.ObjectFactory;
 public class HolidayManagerTest {
 
   @Test
-  public void test() throws JAXBException {
+  public void test() throws JAXBException, MalformedURLException {
 
     ObjectFactory objectFactory = new ObjectFactory();
     Configuration configuration = objectFactory.createConfiguration();
@@ -31,6 +37,7 @@ public class HolidayManagerTest {
     Fixed fixed = objectFactory.createFixed();
     fixed.setDay(1);
     fixed.setMonth(Month.DECEMBER);
+    fixed.setDescriptionPropertiesKey("MEU_FERIADO");
 
     Holidays holidays = objectFactory.createHolidays();
 
@@ -38,15 +45,30 @@ public class HolidayManagerTest {
     holidays.getFixed().add(fixed);
 
     configuration.setHolidays(holidays);
-
+    configuration.setHierarchy("br");
+    configuration.setDescription("Brazil");
 
 
     JAXBContext context = JAXBContext.newInstance(Configuration.class);
     Marshaller jaxbMarshaller = context.createMarshaller();
-
+    jaxbMarshaller.setProperty("com.sun.xml.bind.namespacePrefixMapper", new HolidayPrefixMapper());
     jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+    jaxbMarshaller.setProperty(Marshaller.JAXB_SCHEMA_LOCATION,
+        "http://www.example.org/Holiday /Holiday.xsd");
+
 
     jaxbMarshaller.marshal(root, System.out);
+    jaxbMarshaller.marshal(root, new File("src/test/resources/generated.xml"));
+
+
+    HolidayManager manager = HolidayManager
+        .getInstance(ManagerParameters.create(new URL("file:src/test/resources/generated.xml")));
+
+
+
+    manager.getHolidays(2017).stream().forEach(h -> {
+      System.out.println(h.getDescription());
+    });
 
   }
 
---
+++ src/test/java/inf/kilian/holyday/HolidayPrefixMapper.java
@@ -0,0 +1,21 @@
+package inf.kilian.holyday;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sun.xml.bind.marshaller.NamespacePrefixMapper;
+
+public class HolidayPrefixMapper extends NamespacePrefixMapper {
+
+  private Map<String, String> namespaceMap = new HashMap<>();
+
+  public HolidayPrefixMapper() {
+    namespaceMap.put("http://www.w3.org/2001/XMLSchema-instance", "xsi");
+    namespaceMap.put("http://www.example.org/Holiday", "tns");
+  }
+
+  @Override
+  public String getPreferredPrefix(String namespaceUri, String suggestion, boolean requirePrefix) {
+    return namespaceMap.getOrDefault(namespaceUri, suggestion);
+  }
+}
